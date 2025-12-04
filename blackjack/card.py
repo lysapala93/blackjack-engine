b008719568103c669dabdf1c917fd9fbd11b4b9a
Marco Slawek <marcoslawek@gmx.de>
2025-12-05 00:01:28 +0100

    Some small description

---
+++ blackjack/card.py
@@ -0,0 +1,7 @@
+"""
+card.py
+
+This module defines the Card class used in the Blackjack Simulator. It includes
+Informations about the card itself (color, value) and interprets also the value
+of picture cards.
+"""
