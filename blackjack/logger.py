cf235c0aca6fc0af8d4808d435c3e8b414b99708
Marco Slawek <marcoslawek@gmx.de>
2025-12-04 23:57:09 +0100

    Placeholders

---
+++ blackjack/logger.py
@@ -0,0 +1,16 @@
+import logging
+
+# configure base logger
+logger = logging.getLogger("blackjack")
+logger.setLevel(logging.DEBUG)
+
+# Console Handler
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# Format for logs
+formatter = logging.Formatter("[%(asctime)s.%(msecs)03d]      [%(levelname)-8s]      %(filename)-24s| %(funcName)-27s:  %(lineno)-5d|   %(message)s")
+ch.setFormatter(formatter)
+
+# Handler zum Logger hinzufügen
+logger.addHandler(ch)
