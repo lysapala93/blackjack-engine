c1c7e6671fa413bf99c4425794c82fc05b2cd7c0
Marco Slawek <marcoslawek@gmx.de>
2025-12-08 23:05:59 +0100

    Cleaned up the class

--- blackjack/deck.py
+++ blackjack/deck.py
@@ -5,18 +5,16 @@ from .logger import logger
 
 class Deck:
     def __init__(self, deck_size: int = 6):
-        self._deck_size: int = deck_size
-        self._set_cutcard: bool = False
-        self._pos_cutcard: int = None
-        self._set_end_of_shoe: bool = False
-        self._pos_end_of_shoe: int = None
-        self._deck: list[Card] = []
-        self._shuffled: bool = False
-        self._end_game: bool = False
-
+        self._deck_size = deck_size
         self._deck = self._generate_deck()
 
-    def _generate_deck(self):
+        self._shuffled = False
+        self._cut_position = None
+        self._end_of_shoe_position = None
+        self._end_game = False
+
+        self._initial_cards = len(self._deck)
+
         return [
             Card(suit, rank)
             for _ in range(self._deck_size)
