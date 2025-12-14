92975194547b0ff548f2980674a2950df898571a
Marco Slawek <marcoslawek@gmx.de>
2025-12-14 14:51:46 +0100

    Adapted tests according magic functions

--- tests/test_discard_tray.py
+++ tests/test_discard_tray.py
@@ -5,7 +5,7 @@ from blackjack import DiscardTray
 
 class TestDiscard:
 
-    def test_show_discard():
+    def test_show_discard(self):
         deck = Deck()
         deck.shuffle()
         deck.set_cutcard(pos=37)
@@ -13,40 +13,41 @@ class TestDiscard:
 
         discard_tray = DiscardTray()
 
-        hand = (deck.draw(),)
-        hand = hand + deck.draw()
+        hand = []
+        hand.append(deck.draw())
+        hand.append(deck.draw())
 
         discard_tray.discard(hand)
 
-        assert discard_tray == hand
+        assert discard_tray._discard_deck == hand
 
-    def test_get_number_cards():
+    def test_get_number_cards(self):
         discard_tray = DiscardTray()
         discard_tray.discard(
-            (
+            [
                 Card(suit="Hearts", rank="King"),
                 Card(suit="Diamonds", rank="10"),
                 Card(suit="Hearts", rank="7"),
                 Card(suit="Diamonds", rank="7"),
-            )
+            ]
         )
 
-        assert discard_tray.num_cards == 4
+        assert len(discard_tray) == 4
 
-    def test_reshuffle_to_deck():
+    def test_reshuffle_to_deck(self):
         deck = Deck()
         deck.shuffle()
         deck.set_cutcard(pos=42)
         deck.set_end_of_shoe()
-        num_cards_begin = deck.num_cards_begin
+        num_cards_begin = len(deck)
 
         discard_tray = DiscardTray()
         card = ()
-        for _ in deck.num_cards:
+        for _ in len(deck):
             card = card + deck.draw()
             if deck.end_game == True:
-                discard_tray.add_deck(deck)
+                discard_tray.discard(deck)
                 break
 
-        assert discard_tray.num_cards == 0
-        assert deck.num_cards == num_cards_begin
+        assert len(discard_tray) == 0
+        assert len(deck) == num_cards_begin
