07954eda10901529007e3101d05d11a5b1ac9278
Marco Slawek <marcoslawek@gmx.de>
2025-12-15 21:24:26 +0100

    Developed tests for hand class (TDD)

---
+++ tests/test_handy.py
@@ -0,0 +1,150 @@
+from blackjack import Hand
+from blackjack import Card
+from blackjack import Deck
+from blackjack import DiscardTray
+
+
+class TestHand:
+    def test_role(self):
+        hand_player = Hand(role="player")
+        hand_dealer = Hand(role="dealer")
+
+        assert hand_player.role == "player"
+        assert hand_dealer.role == "dealer"
+
+    def test_value_hand(self):
+        hand_player = Hand(role="player")
+        cards = [Card(suit="Spades", rank="7"), Card(suit="Hearts", rank="Jack")]
+        for card in cards:
+            hand_player.add(card)
+
+        assert hand_player.score == 17
+
+    def test_draw_from_deck(self):
+        hand_player = Hand(role="player")
+        deck = Deck()
+
+        deck.shuffle()
+        initial_size = len(deck)
+        deck.draw()
+
+        hand_player.add(deck.draw())
+        hand_player.add(deck.draw())
+
+        assert len(deck) == initial_size - 3
+
+    def test_ace_value(self):
+        hand_player = Hand(role="player")
+        cards = [Card(suit="Spades", rank="7"), Card(suit="Spades", rank="Ace")]
+
+        for card in cards:
+            hand_player.add(card)
+        score_before = hand_player.score
+        hand_player.add(Card(suit="Hearts", rank="4"))
+        score_after = hand_player.score
+
+        assert score_before == 18
+        assert score_after == 12
+
+    def test_blackjack(self):
+        hand_player = Hand(role="player")
+        cards = [Card(suit="Spades", rank="Jack"), Card(suit="Spades", rank="Ace")]
+
+        for card in cards:
+            hand_player.add(card)
+
+        assert hand_player.blackjack
+        assert not hand_player.bust
+
+    def test_twentyone(self):
+        hand_player = Hand(role="player")
+        cards = [
+            Card(suit="Spades", rank="Jack"),
+            Card(suit="Spades", rank="9"),
+            Card(suit="Hearts", rank="2"),
+        ]
+
+        for card in cards:
+            hand_player.add(card)
+
+        assert not hand_player.blackjack
+        assert not hand_player.bust
+        assert hand_player.score == 21
+
+    def test_bust(self):
+        hand_player = Hand(role="player")
+        cards = [
+            Card(suit="Spades", rank="Jack"),
+            Card(suit="Spades", rank="9"),
+            Card(suit="Hearts", rank="3"),
+        ]
+
+        for card in cards:
+            hand_player.add(card)
+
+        assert not hand_player.blackjack
+        assert hand_player.bust
+
+    def test_discard(self):
+        hand_player = Hand(role="player")
+        deck = Deck()
+        discard_tray = DiscardTray()
+
+        deck.shuffle()
+        initial_size = len(deck)
+        first_discard = deck.draw()
+        discard_tray.discard(first_discard)
+        hand_player.add(deck.draw())
+        hand_player.add(deck.draw())
+
+        hand_player_cards = hand_player.cards
+
+        discard_tray.discard(hand_player.discard())
+
+        assert len(deck) == initial_size - 3
+        assert len(discard_tray) == 3
+        assert hand_player_cards in discard_tray._discard_deck
+        assert first_discard in discard_tray._discard_deck
+
+
+class TestHandPlayer:
+    def test_hand(self):
+        hand_player = Hand(role="player")
+        cards = [Card(suit="Spades", rank="7"), Card(suit="Hearts", rank="Ace")]
+        for card in cards:
+            hand_player.add(card)
+
+        assert hand_player.cards == cards
+
+    def test_split(self):
+        hand_player = Hand(role="player")
+        cards = [Card(suit="Spades", rank="10"), Card(suit="Hearts", rank="Jack")]
+        for card in cards:
+            hand_player.add(card)
+
+        assert hand_player.splitting_possible
+
+        hand_1, hand_2 = hand_player.split()
+
+        assert Card(suit="Spades", rank="10") in hand_1.cards
+        assert Card(suit="Hearts", rank="Jack") in hand_2.cards
+        assert hand_2.active == False
+
+
+class TestHandDealer:
+    def test_hand(self):
+        hand_dealer = Hand(role="dealer")
+        cards = [Card(suit="Spades", rank="Jack"), Card(suit="Spades", rank="Ace")]
+        for card in cards:
+            hand_dealer.add(card)
+
+        assert hand_dealer.cards == [Card(suit="Spades", rank="Jack"), "hidden"]
+
+    def test_hand_revealed(self):
+        hand_dealer = Hand(role="dealer")
+        cards = [Card(suit="Spades", rank="Jack"), Card(suit="Spades", rank="Ace")]
+        for card in cards:
+            hand_dealer.add(card)
+        hand_dealer.reveal()
+
+        assert hand_dealer.cards == cards
