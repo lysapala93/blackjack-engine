601dece14687790a24600efd3d0bb09a42753cd0
Marco Slawek <marcoslawek@gmx.de>
2025-12-16 22:06:11 +0100

    Added simple getters and setters

--- blackjack/hand.py
+++ blackjack/hand.py
@@ -19,3 +19,21 @@ class Hand:
 
         self._role = role
         self._hand: list[Card] = []
+
+    # -----------------------------------------------
+    # properties
+    # -----------------------------------------------
+    @property
+    def role(self) -> str:
+        return self._role
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, name) -> None:
+        self._name = name
+
+        return None
+
