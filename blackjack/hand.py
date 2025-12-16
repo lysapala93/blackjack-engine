3967ba966ffaaf37424cac87e08ef206c26bf626
Marco Slawek <marcoslawek@gmx.de>
2025-12-16 22:05:39 +0100

    Added initialization of class

--- blackjack/hand.py
+++ blackjack/hand.py
@@ -1,2 +1,21 @@
+from blackjack import Card
+from .logger import logger
+from itertools import product
+
+
 class Hand:
-    pass
+
+    def __init__(self, role: str, name: str = None):
+
+        match role:
+            case "player":
+                self._owner_hand: str = name
+
+            case "dealer":
+                self._owner_hand: str = "dealer"
+
+            case _:
+                raise ValueError(f'Unknown role "{role}", please use player or dealer')
+
+        self._role = role
+        self._hand: list[Card] = []
