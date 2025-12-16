b16f1e6d730b8b554a7194c2784f376ca78aef11
Marco Slawek <marcoslawek@gmx.de>
2025-12-16 22:11:19 +0100

    Some renamings and adding of the input "name" (identifier for splitting option)

--- tests/test_handy.py
+++ tests/test_handy.py
@@ -2,18 +2,25 @@ from blackjack import Hand
 from blackjack import Card
 from blackjack import Deck
 from blackjack import DiscardTray
+import pytest
 
 
 class TestHand:
     def test_role(self):
-        hand_player = Hand(role="player")
+        hand_player = Hand(role="player", name="Ocean")
         hand_dealer = Hand(role="dealer")
 
         assert hand_player.role == "player"
         assert hand_dealer.role == "dealer"
 
+    def test_role_negative(self):
+        with pytest.raises(
+            ValueError, match=r'Unknown role "superman", please use player or dealer'
+        ):
+            Hand(role="superman", name="Batman")
+
     def test_value_hand(self):
-        hand_player = Hand(role="player")
+        hand_player = Hand(role="player", name="Ocean")
         cards = [Card(suit="Spades", rank="7"), Card(suit="Hearts", rank="Jack")]
         for card in cards:
             hand_player.add(card)
@@ -21,7 +28,7 @@ class TestHand:
         assert hand_player.score == 17
 
     def test_draw_from_deck(self):
-        hand_player = Hand(role="player")
+        hand_player = Hand(role="player", name="Ocean")
         deck = Deck()
 
         deck.shuffle()
@@ -34,7 +41,7 @@ class TestHand:
         assert len(deck) == initial_size - 3
 
     def test_ace_value(self):
-        hand_player = Hand(role="player")
+        hand_player = Hand(role="player", name="Ocean")
         cards = [Card(suit="Spades", rank="7"), Card(suit="Spades", rank="Ace")]
 
         for card in cards:
@@ -47,7 +54,7 @@ class TestHand:
         assert score_after == 12
 
     def test_blackjack(self):
-        hand_player = Hand(role="player")
+        hand_player = Hand(role="player", name="Ocean")
         cards = [Card(suit="Spades", rank="Jack"), Card(suit="Spades", rank="Ace")]
 
         for card in cards:
@@ -57,7 +64,7 @@ class TestHand:
         assert not hand_player.bust
 
     def test_twentyone(self):
-        hand_player = Hand(role="player")
+        hand_player = Hand(role="player", name="Ocean")
         cards = [
             Card(suit="Spades", rank="Jack"),
             Card(suit="Spades", rank="9"),
@@ -72,7 +79,7 @@ class TestHand:
         assert hand_player.score == 21
 
     def test_bust(self):
-        hand_player = Hand(role="player")
+        hand_player = Hand(role="player", name="Ocean")
         cards = [
             Card(suit="Spades", rank="Jack"),
             Card(suit="Spades", rank="9"),
@@ -86,7 +93,7 @@ class TestHand:
         assert hand_player.bust
 
     def test_discard(self):
-        hand_player = Hand(role="player")
+        hand_player = Hand(role="player", name="Ocean")
         deck = Deck()
         discard_tray = DiscardTray()
 
@@ -97,27 +104,33 @@ class TestHand:
         hand_player.add(deck.draw())
         hand_player.add(deck.draw())
 
-        hand_player_cards = hand_player.cards
+        hand_player_cards = hand_player.hand.copy()
 
         discard_tray.discard(hand_player.discard())
 
         assert len(deck) == initial_size - 3
         assert len(discard_tray) == 3
-        assert hand_player_cards in discard_tray._discard_deck
+        assert all(
+            hand_player_card in discard_tray._discard_deck
+            for hand_player_card in hand_player_cards
+        )
         assert first_discard in discard_tray._discard_deck
+        assert hand_player.score == 0
+        assert not hand_player.blackjack
+        assert not hand_player.bust
 
 
 class TestHandPlayer:
     def test_hand(self):
-        hand_player = Hand(role="player")
+        hand_player = Hand(role="player", name="Ocean")
         cards = [Card(suit="Spades", rank="7"), Card(suit="Hearts", rank="Ace")]
         for card in cards:
             hand_player.add(card)
 
-        assert hand_player.cards == cards
+        assert hand_player.hand == cards
 
     def test_split(self):
-        hand_player = Hand(role="player")
+        hand_player = Hand(role="player", name="Ocean")
         cards = [Card(suit="Spades", rank="10"), Card(suit="Hearts", rank="Jack")]
         for card in cards:
             hand_player.add(card)
@@ -126,9 +139,9 @@ class TestHandPlayer:
 
         hand_1, hand_2 = hand_player.split()
 
-        assert Card(suit="Spades", rank="10") in hand_1.cards
-        assert Card(suit="Hearts", rank="Jack") in hand_2.cards
-        assert hand_2.active == False
+        assert Card(suit="Spades", rank="10") in hand_1.hand
+        assert Card(suit="Hearts", rank="Jack") in hand_2.hand
+        assert hand_player.hand == []
 
 
 class TestHandDealer:
@@ -138,7 +151,7 @@ class TestHandDealer:
         for card in cards:
             hand_dealer.add(card)
 
-        assert hand_dealer.cards == [Card(suit="Spades", rank="Jack"), "hidden"]
+        assert hand_dealer.hand == [Card(suit="Spades", rank="Jack"), "hidden"]
 
     def test_hand_revealed(self):
         hand_dealer = Hand(role="dealer")
