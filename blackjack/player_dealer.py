40927d5c5e765ffcfcbb6674b5dba29749462239
Marco Slawek <marcoslawek@gmx.de>
2026-01-11 12:59:05 +0100

    Added redundant functions in participiant class

--- blackjack/player_dealer.py
+++ blackjack/player_dealer.py
@@ -10,33 +10,28 @@ class Participiant(ABC):
         self._hand: Hand = Hand(self._role, name)
         self._name: str = self._hand.name
 
-    @abstractmethod
-    def hit(self, deck: "Deck") -> "Deck":
-        pass
+    def hit(self, deck: "Deck") -> None:
+        card = deck.draw()
+        self._hand.add(card)
 
-    @abstractmethod
     def stand(self) -> None:
-        pass
+        self._standing = True
 
     @property
-    @abstractmethod
     def role(self) -> str:
-        pass
+        return self._role
 
     @property
-    @abstractmethod
-    def standing(self) -> None:
-        pass
+    def standing(self) -> bool:
+        return self._standing
 
     @property
-    @abstractmethod
     def hand(self) -> "Hand":
-        pass
+        return self._hand
 
     @property
-    @abstractmethod
     def name(self) -> str:
-        pass
+        return self._name
 
 
 class Dealer(Participiant):
@@ -44,60 +39,16 @@ class Dealer(Participiant):
     def __init__(self):
         super().__init__(role="dealer")
 
-    def hit(self, deck: "Deck") -> None:
-        card = deck.draw()
-        self._hand.add(card)
-
-    def stand(self) -> None:
-        self._standing = True
-
-    @property
-    def role(self) -> str:
-        return self._role
-
-    @property
-    def standing(self) -> bool:
-        return self._standing
-
-    @property
-    def hand(self) -> "Hand":
-        return self._hand
-
-    @property
-    def name(self) -> str:
-        return self._name
-
 
 class Player(Participiant):
     def __init__(self, name: str, starting_budget: int):
         super().__init__(role="player", name=name)
         self._budget: int = starting_budget
 
-    def hit(self, deck: "Deck") -> None:
-        card = deck.draw()
-        self._hand.add(card)
-
-    def stand(self) -> None:
-        self._standing = True
-
     def bet(self, amount: int) -> int:
         self._budget -= amount
 
-    @property
-    def role(self) -> str:
-        return self._role
-
-    @property
-    def standing(self) -> bool:
-        return self._standing
-
-    @property
-    def hand(self) -> "Hand":
-        return self._hand
-
-    @property
-    def name(self) -> str:
-        return self._name
+        return amount
 
     @property
     def budget(self) -> int:
